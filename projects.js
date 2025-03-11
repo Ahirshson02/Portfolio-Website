088763a3567b7b20f04659e8ceed2f6cbb9304ea
Adam Hirshson <ahirshson@gmail.com>
2025-03-11 16:31:52 -0400

    Workshop upload step 1

---
+++ projects.js
@@ -0,0 +1,31 @@
+export const projectsInfo = [
+    {
+        projectName: 'C Lite Lexical Analyzer',
+        projectImage: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        projectDesc: 'Lexical analyzer with a GUI for taking in a C Lite file and outputting its symbol table of lexemes and tokens ',
+        projectLink: 'https://github.com/Ahirshson02/Lexical-Analyzer',
+    },
+    {
+        projectName: 'Trivia Game',
+        projectImage: 'https://images.unsplash.com/photo-1573867639040-6dd25fa5f597?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        projectDesc: 'Mobile trivia game built with SwiftUI and a public trivia API. Demonstrates use of asynchronous programming, UI features such as Stacks, Lists, and ScrollViews, and custom structures for parsing JSONs',
+        projectLink: 'https://github.com/Ahirshson02/TriviaGame',
+    },
+    {
+        projectName: 'Kestra: Open Source Contributions',
+        projectImage: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        projectDesc: 'Contributed to a large open source project by completing the language translation feature and having it include a broader scope of the web application to allow for a more complete translation ',
+        projectLink: 'https://github.com/Ahirshson02/kestraFork',
+    },{
+        projectName: 'WhatToDo',
+        projectImage: 'https://images.unsplash.com/photo-1642132652859-3ef5a1048fd1?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        projectDesc: 'Mobile app built with Dart, its Flutter framework, and Supabase. Designed and implemented as a team, WhatToDo combines Google Maps and TicketMaster APIs to show users public events near them, as well as the ability to create and publize their own custom events',
+        projectLink: 'https://github.com/KarlProgs/cen_group_project',
+    },
+    {
+        projectName: 'Some project',
+        projectImage: 'https://images.unsplash.com/photo-1642132652860-471b4228023e?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        projectDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ',
+        projectLink: 'https://github.com/',
+    }
+]
